089ab09dce1c70d6a524626ec9ecffb5dfeff020
andela-uenyingwa <uloaku.enyingwa@andela.com>
2017-02-16 03:08:42 +0100

    modify js files

--- src/js/inverted-index-utilities.js
+++ src/js/inverted-index-utilities.js
@@ -6,11 +6,11 @@ class InvertedIndexUtilities {
   /**
    * cleanString
    * removes unwanted punctuations from string
-   * @param{String} string string to remove unwanted punctuations from
+   * @param{String} content string to remove unwanted punctuations from
    * @return {Array} an array of valid string
    */
-  static cleanString(string) {
-    const validCharacters = string.replace(/[^\w+\s]/gi, '')
+  static cleanString(content) {
+    const validCharacters = content.replace(/[^\w+\s]/gi, '')
     .toLowerCase()
     .split(' ');
     return validCharacters;
@@ -19,29 +19,29 @@ class InvertedIndexUtilities {
   /**
    * getTokens
    * gets an array of tokens from a string
-   * @param {String} string string to generate token from
+   * @param {String} content string to generate token from
    * @return {Array} an array of generated token
    */
-  static getTokens(string) {
-    const tokens = this.cleanString(string);
+  static getTokens(content) {
+    const tokens = this.cleanString(content);
     return Array.from(new Set(tokens));
   }
 
   /**
    * validateData
    * Reads content of JSON and checks for validity
-   * @param {Object} data content of JSON to check for validity
+   * @param {Object} file content of JSON to check for validity
    * @return {Boolean} validity status of the JSON content
    */
-  static validateData(data) {
+  static validateData(file) {
     let status = true;
 
-    if (!Array.isArray(data) || data.length === 0) {
+    if (!Array.isArray(file) || file.length === 0) {
       status = false;
     }
 
     try {
-      data.forEach((currentDoc) => {
+      file.forEach((currentDoc) => {
         const hasTitle = currentDoc.title;
         const hasText = currentDoc.text;
         if (!hasTitle && !(hasText)) {
